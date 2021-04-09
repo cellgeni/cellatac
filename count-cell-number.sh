c82c8ada66df9380209e5d25fb28bcdf4938b72c
Simon Murray <simondonaldmurray@gmail.com>
2021-04-09 17:11:56 +0100

    added script to calculate cell number

---
+++ count-cell-number.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+
+manifest=${1?Please provide manifest file}
+
+totalcounts=0
+while read sampleid path;
+  do
+    thefile="$path/singlecell.csv"
+    if [[ ! -e $thefile ]]
+    then 
+      echo "singlecell.csv not present for sample" $sampleid && false
+    else
+      counts=$(python get-col.py -i $thefile -s "," -c "is__cell_barcode" -f "1" -o "," | wc -l)
+      echo $sampleid $counts
+      ((totalcounts+=counts))
+    fi;
+done < <(cut -f 2,3 $manifest)
+echo "total:" $totalcounts

ce93c09973521db6491ac1475d731275f931a1f6
Stijn van Dongen <micans@users.noreply.github.com>
2021-04-10 15:00:38 +0100

    shorter name, use shebang

--- bin/count-cells.sh
+++ bin/count-cells.sh
@@ -12,7 +12,7 @@ while read sampleid path;
     then 
       echo "singlecell.csv not present for sample" $sampleid && false
     else
-      counts=$(python get-col.py -i $thefile -s "," -c "is__cell_barcode" -f "1" -o "," | wc -l)
+      counts=$(get-col.py -i $thefile -s "," -c "is__cell_barcode" -f "1" -o "," | wc -l)
       echo $sampleid $counts
       ((totalcounts+=counts))
     fi;

450631c4c6868f379f1dde5f9d41aa8f4342deb0
Stijn van Dongen <micans@users.noreply.github.com>
2021-04-13 20:51:26 +0100

    explain iota

--- bin/versionsort-sam-header.sh
+++ bin/versionsort-sam-header.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Dual gex+atac sequencing delivers the contigs in lexicographic sort order.
-# As we combine them with regular atac data and are used to version sort order,
-# this script is used to re-sort the header of the sam file.  Note that we sort
-# the fragment data as well in the get scripts (external to cellatac), using
+# As we sometimes combine them with regular atac data and are used to version
+# sort order, this script is used to re-sort the header of the sam file.  Note
+# that we sort the fragment data as well in the data-copying scripts (external
+# to cellatac), using
 #
 # zcat $frag | sort --parallel=4 -k 1,1V -k 2,2n -k 3,3n | gzip > fragments.tsv.gz
 
